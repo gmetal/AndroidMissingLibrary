341fc07130f4885337a3e4b7039c78f56321c600
gmetaxas <gmetaxas@globoplc.com>
2016-01-18 20:22:33 +0200

    Added IntentBuilder

---
+++ missinglib/src/main/java/gr/kcodex/missinglib/builders/IntentBuilder.java
@@ -0,0 +1,58 @@
+package gr.kcodex.missinglib.builders;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Parcelable;
+
+public class IntentBuilder {
+    private Intent mIntent;
+
+    private IntentBuilder() {
+        mIntent = new Intent();
+    }
+
+    public static IntentBuilder create() {
+        return new IntentBuilder();
+    }
+
+    public IntentBuilder setAction(String action) {
+        mIntent.setAction(action);
+        return this;
+    }
+
+    public IntentBuilder setData(Uri data) {
+        mIntent.setData(data);
+        return this;
+    }
+
+    public IntentBuilder setClass(Context ctx, Class<?> cls) {
+        mIntent.setClass(ctx, cls);
+        return this;
+    }
+
+    public IntentBuilder setPackage(String pkg) {
+        mIntent.setPackage(pkg);
+        return this;
+    }
+
+    public IntentBuilder putExtra(String name, Parcelable value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public IntentBuilder putExtra(String name, String value) {
+        mIntent.putExtra(name, value);
+        return this;
+    }
+
+    public IntentBuilder addFlags(int flags) {
+        mIntent.addFlags(flags);
+        return this;
+    }
+
+    public Intent build() {
+        return mIntent;
+    }
+}
+
